d4a3e46b35c2cd99c478c8c34d16ce8a49f53f58
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2022-09-14 20:52:25 -0300

    #dev.01 - add first entities

---
+++ src/Loth.Business/Models/Fornecedores/Fornecedor.cs
@@ -0,0 +1,13 @@
+﻿using Loth.Business.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Business.Models.Fornecedores
+{
+    public class Fornecedor : Entity
+    {
+    }
+}
