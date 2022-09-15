9d1f996f5260c7e907a06d2148099c9a9f1e6a3e
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2022-09-14 21:22:30 -0300

    #dev.01 - interfaces for dataacess

---
+++ src/Loth.Business/Models/Fornecedores/IEnderecoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Loth.Business.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Business.Models.Fornecedores
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
---
+++ src/Loth.Business/Models/Produtos/IProdutorepository.cs
@@ -0,0 +1,16 @@
+﻿using Loth.Business.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Business.Models.Produtos
+{
+    public interface IProdutorepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterProdutosPorfornecedor(Guid fornecedorId);
+        Task<IEnumerable<Produto>> ObterProdutosFornecedores();
+        Task<Produto> ObterProdutoFornecedor(Guid id);
+    }
+}
