c381df475ad9d6d4b9a8368f730f9e5a11419774
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2022-09-15 10:54:40 -0300

    #dev.01 - Config DbContext

---
+++ src/Loth.Infra/Data/Context/MeuDbContex.cs
@@ -0,0 +1,21 @@
+﻿using Loth.Business.Models.Fornecedores;
+using Loth.Business.Models.Produtos;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Infra.Data.Context
+{
+    public class MeuDbContex : DbContext
+    {
+        public MeuDbContex() : base("DefaultConnection")
+        {}
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
