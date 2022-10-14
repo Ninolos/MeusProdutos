eabc1b227b658f04c1a60fdea7a724624c42ce22
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2022-10-13 21:16:57 -0300

    #dev.01 - Image Upload Front-End side

--- src/Loth.AppMvc/ViewModels/ProdutoViewModel.cs
+++ src/Loth.AppMvc/ViewModels/ProdutoViewModel.cs
@@ -31,8 +31,8 @@ namespace Loth.AppMvc.ViewModels
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
-        //[DisplayName("Imagem do Produto")]
-        //public HttpPostedFileBase ImagemUpload { get; set; }
+        [DisplayName("Imagem do Produto")]
+        public HttpPostedFileBase ImagemUpload { get; set; }
 
         [DisplayName("Imagem do Produto")]
         public string Imagem { get; set; }
